43bd6a483f5017072a33babfb26bbdf00ac9d1ce
github username <github email address>
2019-05-24 09:59:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(street) {
+  const hdqr = 42;
+  if (street > hdqr) {
+    return street - hdqr;
+  }
+  else {
+    return hdqr - street;
+  }
+}
